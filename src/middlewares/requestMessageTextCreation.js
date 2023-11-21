eb793474ff014910ec8d6e3c4247d72c05616215
Volodymyr Dudka <tanetti22@gmail.com>
2023-11-21 16:17:33 +0200

    Fix ortho

--- src/middlewares/requestMessageTextCreation.js
+++ src/middlewares/requestMessageTextCreation.js
@@ -26,7 +26,7 @@ const requestMessageTextCreation = (req, res, next) => {
     \n<i> - Ім'я:</i>  <b>${name} ${surename}</b>\n<i> - Телефон:</i>  <a href="tel:${cleanPhoneNumber}">${cleanPhoneNumber}</a>
     \n<i> - Дата народження:</i>  <b>${dateString}</b>\n<i> - Вік:</i>  <b>${ageString}</b>
     \n<i> - Зріст:</i>  <b>${height} см.</b>\n<i> - Вага:</i>  <b>${weight} кг.</b>
-    \n<i> - Дозіл лікаря:</i>  ${medical ? "\u2705" : "\u274C"}`;
+    \n<i> - Дозвіл лікаря:</i>  ${medical ? "\u2705" : "\u274C"}`;
 
   next();
 };
